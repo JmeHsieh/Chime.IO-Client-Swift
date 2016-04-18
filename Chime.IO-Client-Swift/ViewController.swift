a2b2e51645ae2fc4fa625aec4953f4c29aa1739e
JmeHsieh <jmehsieh@gmail.com>
2016-04-18 12:08:48 +0800

    Initial Commit

---
+++ Chime.IO-Client-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chime.IO-Client-Swift
+//
+//  Created by JmeHsieh on 4/18/16.
+//  Copyright © 2016 JmeHsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
