577fd27968ee8637b95eba938c09131f752af0ee
JmeHsieh <jmehsieh@gmail.com>
2016-04-22 15:36:42 +0800

    Add RxSwift into cartfile

--- Chime.IO-Client-Swift/ViewController.swift
+++ Chime.IO-Client-Swift/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        return
+        ChimeIOAPI.sharedInstance.connect().then {
+            return ChimeIOAPI.sharedInstance.login("jme_1@email.com", "00000000")
+        }.then { (jwt, user) -> Promise<[Room]> in
+            print("jwt: \(jwt)")
+            print("user: \(user.username)")
+            return ChimeIOAPI.sharedInstance.getMyRooms()
+        }.then { rooms -> Promise<Room> in
+            print(rooms)
+            return ChimeIOAPI.sharedInstance.getRoom(rooms[0].id)
+        }.then { room -> Promise<Message> in
+            print(room)
+            return ChimeIOAPI.sharedInstance.postMessage("today is a good day", inRoomID: room.id)
+        }.then{ message in
+            print(message)
+        }.error { error in
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
