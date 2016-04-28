27d8158c3d6e589e69a880394af0e724f135a3a6
JmeHsieh <jmehsieh@gmail.com>
2016-04-28 15:33:52 +0800

    Create SharingManager for sharing data across the app.

---
+++ Chime.IO-Client-Swift/SharingManager.swift
@@ -0,0 +1,15 @@
+//
+//  SharingManager.swift
+//  Chime.IO-Client-Swift
+//
+//  Created by JmeHsieh on 4/28/16.
+//  Copyright © 2016 JmeHsieh. All rights reserved.
+//
+
+import Foundation
+
+class SharingManager {
+    
+    static let defaultManager = SharingManager()
+    var currentUserInfo: (String, User)?
+}
