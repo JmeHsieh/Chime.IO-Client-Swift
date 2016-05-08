fff216e6c05ba6892122e33bd517ce7475ff6177
JmeHsieh <jmehsieh@gmail.com>
2016-05-08 16:39:34 +0800

    Add signup option for login page.

--- Chime.IO-Client-Swift/LoginViewController.swift
+++ Chime.IO-Client-Swift/LoginViewController.swift
@@ -14,12 +14,21 @@ import SnapKit
 
 class LoginViewController: UIViewController {
     
+    // ui
     var emailTextField: UITextField!
     var passwordTextField: UITextField!
-    var sendButton: UIButton!
     var spinner: UIActivityIndicatorView!
+    var sendButton: UIButton!
+    var signAlert: UIAlertController!
     
+    // rx
+    private let disposeBag = DisposeBag()
+    private let sendV = Variable<String>("")
+    var sendD: Driver<String>?
     var viewModel: LoginViewModel!
+    
+    
+    // MARK: - Constructros
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -29,10 +38,14 @@ class LoginViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.whiteColor()
         
+        // setup views
         emailTextField = UITextField()
         emailTextField.borderStyle = .RoundedRect
         emailTextField.textColor = UIColor.blackColor()
@@ -47,7 +60,7 @@ class LoginViewController: UIViewController {
         passwordTextField.textColor = UIColor.blackColor()
         passwordTextField.placeholder = "password"
         passwordTextField.secureTextEntry = true
-        passwordTextField.returnKeyType = .Send
+        passwordTextField.returnKeyType = .Done
         
         spinner = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
         spinner.hidesWhenStopped = true
@@ -58,12 +71,16 @@ class LoginViewController: UIViewController {
         sendButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
         sendButton.setTitleColor(UIColor(white: 0, alpha: 0.2), forState: .Disabled)
         
+        signAlert = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        signAlert.addAction(UIAlertAction(title: "Login", style: .Default) { [unowned self] in self.sendV.value = $0.title! })
+        signAlert.addAction(UIAlertAction(title: "Signup", style: .Default) { [unowned self] in self.sendV.value = $0.title! })
+        signAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
         view.addSubview(spinner)
         view.addSubview(sendButton)
         
-        // constraints
         emailTextField.snp_makeConstraints {
             $0.top.equalTo(view).offset(160)
             $0.centerX.equalTo(view)
@@ -89,11 +106,30 @@ class LoginViewController: UIViewController {
             $0.centerX.equalTo(passwordTextField)
         }
         
+        
         // rx
+        emailTextField.rx_controlEvent(.EditingDidEndOnExit)
+            .asDriver()
+            .driveNext { [unowned self] _ in
+                self.passwordTextField.becomeFirstResponder()
+            }.addDisposableTo(disposeBag)
+        
+        passwordTextField.rx_controlEvent(.EditingDidEnd)
+            .asDriver()
+            .driveNext { [unowned self] _ in
+            self.passwordTextField.resignFirstResponder()
+        }.addDisposableTo(disposeBag)
+        
+        sendButton.rx_tap.asDriver().driveNext { [unowned self] _ in
+            self.presentViewController(self.signAlert, animated: true, completion: nil)
+        }.addDisposableTo(disposeBag)
+        
+        sendD = sendV.asDriver()
+        
         viewModel = LoginViewModel(email: emailTextField.rx_text.asDriver(),
                                    password: passwordTextField.rx_text.asDriver(),
                                    spinnerAnimating: spinner.rx_animating,
-                                   sendButtonTap: sendButton.rx_tap.asDriver(),
+                                   send: sendD!,
                                    sendButtonEnabled: sendButton.rx_enabled)
         
         viewModel.loginOA.subscribeNext {
@@ -105,14 +141,34 @@ class LoginViewController: UIViewController {
             } else {
                 print("login failed")
             }
-        }.addDisposableTo(viewModel.disposeBag)
+        }.addDisposableTo(disposeBag)
         
         viewModel.loginOA.subscribeCompleted {
             print("go to next page")
             dispatch_async(dispatch_get_main_queue()) {
+                print(self.navigationController)
                 self.navigationController?.pushViewController(RoomsViewController(), animated: true)
             }
-        }.addDisposableTo(viewModel.disposeBag)
+        }.addDisposableTo(disposeBag)
+        
+        viewModel.signupOA.subscribeNext {
+            if let userinfo = $0 {
+                print("signup succeed")
+                print("jwt \(userinfo.0)")
+                print("user \(userinfo.1)")
+                SharingManager.defaultManager.currentUserInfo = userinfo
+            } else {
+                print("signup failed")
+            }
+        }.addDisposableTo(disposeBag)
+        
+        viewModel.signupOA.subscribeCompleted {
+            print("go to next page")
+            dispatch_async(dispatch_get_main_queue()) {
+                print(self.navigationController)
+                self.navigationController?.pushViewController(RoomsViewController(), animated: true)
+            }
+        }.addDisposableTo(disposeBag)
     }
     
     override func didReceiveMemoryWarning() {
--- Chime.IO-Client-Swift/LoginViewModel.swift
+++ Chime.IO-Client-Swift/LoginViewModel.swift
@@ -12,30 +12,31 @@ import RxSwift
 
 class LoginViewModel {
     
+    private let disposeBag = DisposeBag()
     private let isloadingV = Variable<Bool>(false)
-    lazy var disposeBag = DisposeBag()
     
     // ui
     var emailD: Driver<String>
     var passwordD: Driver<String>
-    var sendButtonTapD: Driver<Void>
+    var sendD: Driver<String>
     var spinnerAnimatingO: AnyObserver<Bool>
     var sendButtonEnabledO: AnyObserver<Bool>
     
     // login result
-    typealias LoginResult = (String, User)
-    var loginOA: Observable<LoginResult?>!
+    typealias Result = (String, User)
+    var loginOA: Observable<Result?>!
+    var signupOA: Observable<Result?>!
     
     
     init(email: Driver<String>,
          password: Driver<String>,
          spinnerAnimating: AnyObserver<Bool>,
-         sendButtonTap: Driver<Void>,
+         send: Driver<String>,
          sendButtonEnabled: AnyObserver<Bool>) {
         emailD = email
         passwordD = password
         spinnerAnimatingO = spinnerAnimating
-        sendButtonTapD = sendButtonTap
+        sendD = send
         sendButtonEnabledO = sendButtonEnabled
         
         let isloadingD = isloadingV.asDriver().distinctUntilChanged()
@@ -53,15 +54,37 @@ class LoginViewModel {
             .addDisposableTo(disposeBag)
         
         // login result as observable
-        loginOA = Observable.create { [weak self] o in
-            guard let `self` = self else { return NopDisposable.instance }
-            return self.sendButtonTapD
+        loginOA = Observable.create { [unowned self] o in
+            return self.sendD
+                .filter { $0 == "Login" }
                 .withLatestFrom(isloadingD)
                 .filter { !$0 }
                 .withLatestFrom(Driver.combineLatest(self.emailD, self.passwordD) { ($0, $1) })
                 .driveNext {
                     self.isloadingV.value = true
                     ChimeIOAPI.sharedInstance.login($0, $1).then { (jwt, user) -> Void in
+                        self.isloadingV.value = false
+                        o.onNext((jwt, user))
+                        o.onCompleted()
+                    }.error { e in
+                        self.isloadingV.value = false
+                        o.onNext(nil)
+                    }
+            }
+        }.shareReplayLatestWhileConnected()
+        
+        // signup result as observable
+        signupOA = Observable.create { [unowned self] o in
+            return self.sendD
+                .filter { $0 == "Signup" }
+                .withLatestFrom(isloadingD)
+                .filter { !$0 }
+                .withLatestFrom(Driver.combineLatest(self.emailD, self.passwordD) {
+                    ($0.componentsSeparatedByString("@").first ?? "your username", $0, $1)
+                })
+                .driveNext {
+                    self.isloadingV.value = true
+                    ChimeIOAPI.sharedInstance.signup($0, $1, $2).then { (jwt, user) -> Void in
                         self.isloadingV.value = false
                         o.onNext((jwt, user))
                         o.onCompleted()
